5a0e81549e6023f71f361f2591e56a8a08d863dd
mvaude <maxime.vaude@gmail.com>
2022-06-05 22:51:12 +0200

    refactor(treesitter): change config

--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -14,15 +14,16 @@ require('nvim-treesitter.configs').setup({
     'gowork',
     'graphql',
     'hcl',
-    'hjson',
     'html',
     'http',
     'javascript',
     'jsdoc',
     'json',
     'latex',
+    'markdown',
     'python',
     'ql',
+    'query',
     'regex',
     'rust',
     'scss',
@@ -43,16 +44,10 @@ require('nvim-treesitter.configs').setup({
     -- `false` will disable the whole extension
     enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { 'c', 'rust' },
+    use_languagetree = false,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    disable = { 'json' },
+
+    -- additional_vim_regex_highlighting = false,
   },
 })
